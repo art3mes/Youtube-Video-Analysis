d0f6b8fc1b00e351f030dd5f2ba3e97d22fe5a9d
art3mes <93867313+art3mes@users.noreply.github.com>
2023-12-04 13:11:12 +0530

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,19 @@ import React from "react";
 // import RefreshIcon from '@mui/icons-material/Refresh';
 
 function Footer() {
-  const year = new Date().getFullYear();
+  //const year = new Date().getFullYear();
   // const openInNewTab = url => {
   //   window.open(url, '_blank', 'noopener,noreferrer');
   // };
   return (
     <div>
         <footer>
-          <p>art3mes ⓒ {year}</p>
+          {/* <p>art3mes ⓒ {year}</p> */}
+          <div className="footer-text">
+            <div>Afreen Qamar</div>
+            <div>Mohammad Ali</div>
+            <div>Mohammad Shoaib</div>
+          </div>
         </footer>
         {/* <button className="refreshButton" onClick={() => openInNewTab('https://keeper-app-backend-dyr1.onrender.com/')}><RefreshIcon /></button> */}
     </div>
